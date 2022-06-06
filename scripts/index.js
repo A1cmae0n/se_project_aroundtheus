f72b3e41f43d35b895b2e2da4e7aae122ce98710
a1cmae0n <jmulmer@gmail.com>
2022-06-05 20:14:30 -0700

    feat: add ability to update name and description

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,37 @@
 
 const profileEdit = document.querySelector(".profile__edit");
 const modal = document.querySelector(".modal");
+const formButton = document.querySelector(".form__button");
+// const modalContainer = document.querySelector(".modal__container");
 const modalCloseIcon = document.querySelector(".modal__close");
 
-let userName = document.querySelector(".profile__name").textContent;
-let userDescription = document.querySelector(".profile__title").textContent;
+let userName = document.querySelector(".profile__name");
+let userDescription = document.querySelector(".profile__title");
 
 
 profileEdit.addEventListener("click", openModal);
 modalCloseIcon.addEventListener("click", closeModal);
+formButton.addEventListener("click", saveProfileEdit);
+
 
 
 function openModal() {
-    modal.style.visibility = "visible";
-    document.querySelector(".form__input-name").placeholder = userName;
-    document.querySelector(".form__input-description").placeholder = userDescription;
+    modal.classList.add("modal_opened");
+    // modal.classList.toggle(".modal_opened");
+    document.querySelector(".form__input-name").placeholder = userName.textContent;
+    document.querySelector(".form__input-description").placeholder = userDescription.textContent;
 }
 
 function closeModal() { 
-    modal.style.visibility = "hidden";
+    // modal.classList.remove(".modal_opened")
+    modal.classList.remove("modal_opened");
+}
+
+function saveProfileEdit(e){
+    e.preventDefault();
+    let userNameInput = document.getElementById("name").value;
+    let userDescriptionInput = document.getElementById("description").value;
+    userName.textContent = userNameInput;
+    userDescription.textContent = userDescriptionInput;
+    closeModal();
 }
